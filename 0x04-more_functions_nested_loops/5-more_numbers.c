75661feb4f23006977e764a79f40ff2cb9be8305
dag14 <dagimasrat3@gmail.com>
2022-06-09 11:38:13 -0700

    more case

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,26 @@
 #include "main.h"
-/*** print_most_numbers
-* Return: Always 0.
+/**
+ * more_numbers - function that checks for uppercase character
+ * Return: 0
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
-char c = '0';
 
-while (c <= '9')
+int a = 0;
+int b = 0;
+
+while (b <= 9)
 {
-if (c != '2' && c != '4')
-{
-_putchar(c);
-}
-c++;
+while (a <= 14)
+	{
+if (a > 9)
+_putchar(a / 10 + '0');
+_putchar(a % 10 + '0');
+a++;
+
 }
 _putchar('\n');
+b++;
+a = 0;
 }
-
+}
