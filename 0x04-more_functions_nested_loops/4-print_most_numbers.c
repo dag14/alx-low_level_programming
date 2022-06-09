7e3b7d157cc31adbef197fb5e923cfd38fb88d7a
dag14 <dagimasrat3@gmail.com>
2022-06-09 11:29:13 -0700

    prints

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*** print_most_numbers
+* Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+char c = '0';
+
+while (c <= '9')
+{
+if (c != '2' && c != '4')
+{
+_putchar(c);
+}
+c++;
+}
+_putchar('\n');
+}
